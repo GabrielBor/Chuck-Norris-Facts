6e75b6a0df433481388223c1a5d57b8e1156590a
Gabriel de Sousa Borges <tqi_gborges@uolinc.com>
2020-04-13 09:34:41 -0300

    Create repository

---
+++ ChuckNorrisFacts/ChuckNorrisFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorrisFacts
+//
+//  Created by Gabriel Borges on 13/04/20.
+//  Copyright © 2020 Gabriel Borges. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
